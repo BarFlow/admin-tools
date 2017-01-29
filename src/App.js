207f773fa89da3cb3cdf385c68f76575341ec098
Peter Gombos <pepegombos@gmail.com>
2017-01-29 17:27:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react'
+
+import Measure from './components/Measure'
+import './App.css'
+
+const apiUrl = 'https://api.stockmate.co.uk'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      skip: parseInt(localStorage.getItem('skip'), 10) || 0,
+      token: localStorage.getItem('token') || ''
+    }
+    this.next = this.next.bind(this)
+    this.back = this.back.bind(this)
+    this.fetchCurrentProduct = this.fetchCurrentProduct.bind(this)
+    this.updateProduct = this.updateProduct.bind(this)
+    this.login = this.login.bind(this)
+  }
+
+  componentDidMount () {
+    if (this.state.token) {
+      this.fetchCurrentProduct()
+    }
+  }
+
+  fetchCurrentProduct () {
+    const req = new Request(`${apiUrl}/products?measurable=true&limit=1&skip=${this.state.skip}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${this.state.token}`
+      }
+    })
+    fetch(req)
+      .then(response => response.json())
+      .then(products => this.setState({ product: products[0] }))
+  }
+
+  updateProduct (product) {
+    const req = new Request(`${apiUrl}/products/${product._id}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${this.state.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+    return fetch(req)
+  }
+
+  next (measures) {
+    const payload = {
+      _id: this.state.product._id, ...measures
+    }
+    this.updateProduct(payload)
+    .then(() => {
+      localStorage.setItem('skip', this.state.skip + 1)
+      this.setState({
+        skip: this.state.skip + 1
+      }, () => this.fetchCurrentProduct())
+    })
+  }
+
+  back () {
+    localStorage.setItem('skip', this.state.skip - 1)
+    this.setState({
+      skip: this.state.skip - 1
+    }, () => this.fetchCurrentProduct())
+  }
+
+  login () {
+    const req = new Request(`${apiUrl}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${this.state.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
+    })
+    return fetch(req)
+      .then((res) => res.json())
+      .then(json => {
+        this.setState({
+          token: json.token
+        }, () => this.fetchCurrentProduct())
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        {!this.state.token &&
+          <div>
+            <input type='text' onChange={(e) => this.setState({
+              email: e.currentTarget.value
+            })} />
+            <input type='password' onChange={(e) => this.setState({
+              password: e.currentTarget.value
+            })} />
+            <button onClick={this.login}>Log-in</button>
+          </div>
+        }
+        {this.state.product &&
+        <Measure
+          back={this.back}
+          next={this.next}
+          product={this.state.product} />
+        }
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Measure/Measure.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react'
+
+import './Measure.css'
+
+class Measure extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      measurable_from: 0.2,
+      measurable_till: 0.8,
+      error: {}
+    }
+
+    this._handleNext = this._handleNext.bind(this)
+    this.onMousedown = this.onMousedown.bind(this)
+    this.onMousemove = this.onMousemove.bind(this)
+    this.onMouseup = this.onMouseup.bind(this)
+  }
+
+  componentDidMount () {
+    this.setState({
+      measurable_from: this.props.product.measurable_from,
+      measurable_till: this.props.product.measurable_till
+    })
+    var canvas = this.refs.canvas
+    canvas.addEventListener('mousemove', this.onMousemove, false)
+    document.addEventListener('mouseup', this.onMouseup, false)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.product !== nextProps.product) {
+      this.setState({
+        measurable_from: nextProps.product.measurable_from,
+        measurable_till: nextProps.product.measurable_till
+      })
+    }
+  }
+
+  onMousemove (e) {
+    let mPosy = 0
+    let ePosy = 0
+    let obj = this.refs.canvas
+    // get mouse position on document crossbrowser
+    if (!e) { e = window.event }
+    if (e.pageX || e.pageY) {
+      mPosy = e.pageY - document.body.scrollTop
+    } else if (e.clientX || e.clientY) {
+      mPosy = e.clientY + document.documentElement.scrollTop
+    }
+    // get parent element position in document
+    if (obj.offsetParent) {
+      do {
+        ePosy += obj.offsetTop - obj.scrollTop
+        // eslint-disable-next-line
+      } while (obj = obj.offsetParent)
+    }
+    // mouse position minus elm position is mouseposition relative to element:
+    if (this.activeLine) {
+      this.activeLine.pos = 1 - ((mPosy - ePosy - 1) / this.refs.canvas.offsetHeight)
+      this.activeLine.style['top'] = (mPosy - ePosy - 1) + 'px'
+    }
+  }
+
+  onMouseup (e) {
+    if (this.activeLine) {
+      this.setState({
+        [this.activeLine.id]: this.activeLine.pos
+      })
+      this.activeLine = null
+    }
+  }
+
+  onMousedown (e) {
+    this.activeLine = e.currentTarget
+  }
+
+  _handleNext () {
+    const { next } = this.props
+    next({
+      measurable_from: this.state.measurable_from,
+      measurable_till: this.state.measurable_till
+    })
+  }
+
+  render () {
+    const { back, product } = this.props
+    const { measurable_from: measurableFrom, measurable_till: measurableTill } = this.state
+    return (
+      <div className='measure'>
+        <div ref='canvas' className='canvas' style={{ backgroundImage: `url(${product.images.normal})` }}>
+          <div
+            id='measurable_till'
+            onMouseDown={this.onMousedown}
+            style={{ top: (1 - measurableTill) * 100 + '%' }}
+            className='line'>
+            <span>Max.</span>
+            <span className='pull-right' />
+          </div>
+          <div
+            id='measurable_from'
+            onMouseDown={this.onMousedown}
+            style={{ top: (1 - measurableFrom) * 100 + '%' }}
+            className='line'>
+            <span>Min.</span>
+            <span className='pull-right' />
+          </div>
+        </div>
+        <div className='product-create-footer'>
+          <button onClick={back}>
+            Back
+          </button>
+          <button onClick={this._handleNext} className='pull-right'>
+            Save and Next
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+Measure.propTypes = {
+  back: React.PropTypes.func.isRequired,
+  next: React.PropTypes.func.isRequired,
+  product: React.PropTypes.object
+}
+
+export default Measure
